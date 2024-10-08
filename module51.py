d456394dd70a140915777a6b66767c257ac323d0
Clattonia <128783736+Clattonia@users.noreply.github.com>
2024-10-08 13:56:19 +0500

    Add files via upload

---
+++ module51.py
@@ -0,0 +1,16 @@
+class house:
+    def __init__(self,name,floors):
+        self.name = name
+        self.floors = floors
+        pass
+    def go_to(self, new_floor):
+        if new_floor > self.floors:
+            print('Неправильно,попробуй еще раз')
+        else:
+            for i in range(new_floor):
+                i += 1
+                print(i)
+          
+
+h1 = house("ЖК", 5)
+h1.go_to(6)
